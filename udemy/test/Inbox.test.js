3fc7a490a69b4be1c3bd9eec583ff4aac05690be
Pablo F. Mescher <pfmescher@gmail.com>
2019-07-03 16:08:52 -0300

    Added setMessage test

--- udemy/test/Inbox.test.js
+++ udemy/test/Inbox.test.js
@@ -7,15 +7,20 @@ const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
+let defaults = {
+    from: null,
+    gas: 1000000
+};
 
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
+    defaults.from = accounts[0];
 
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!'] })
-        .send({ from: accounts[0], gas: 1000000 })
+        .send(defaults)
 
 });
 
@@ -27,5 +32,14 @@ describe('Inbox', () => {
    it('has a default message', async () => {
        const message = await inbox.methods.message().call();
        assert.equal(message, 'Hi there!');
+   });
+
+   it('can change the message', async () => {
+      const newMessage = 'Bye there!';
+      await inbox.methods.setMessage(newMessage)
+          .send(defaults);
+      const changed = await inbox.methods.message().call();
+
+      assert.equal(changed, newMessage);
    });
 });
